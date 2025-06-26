4bd4a9a30611d666e329a1c6406bc2693b8eee5c
DiegoNicolas1992 <cuellodiego333@gmail.com>
2025-06-26 11:42:10 -0300

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,15 +1,11 @@
 function cargarNavbar() {
-  const paginas = [
-    { titulo: "Home", url: "index.html" },
-    { titulo: "Electrónica", url: "electronica.html" },
-    { titulo: "Ropa", url: "ropa.html" },
-    { titulo: "Hogar", url: "hogar.html" },
-    { titulo: "Logout", url: "login.html" }
-  ];
-
-  const nav = document.getElementById("menuNav");
-  nav.innerHTML = `
-    <span class="logo">🛒 Tienda Nova</span>
-    ${paginas.map(p => `<a href="${p.url}">${p.titulo}</a>`).join('')}
-  `;
+  fetch("data/menu.json")
+    .then(res => res.json())
+    .then(menu => {
+      const nav = document.getElementById("menuNav");
+      nav.innerHTML = `
+        <span class="logo">🛒 Tienda Nova</span>
+        ${menu.map(item => `<a href="${item.url}">${item.titulo}</a>`).join("")}
+      `;
+    });
 }
