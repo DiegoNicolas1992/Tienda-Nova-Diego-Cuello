7e79f7bc8e95f38a36fdf83e2d3a2684391305bc
DiegoNicolas1992 <cuellodiego333@gmail.com>
2025-06-27 17:48:52 -0300

    Update navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,16 +3,10 @@ function cargarNavbar() {
   nav.innerHTML = `
     <ul class="navbar">
       <li><a href="index.html">Inicio</a></li>
-      <li><a href="electronica.html">Electrónica</a></li>
       <li><a href="ropa.html">Ropa</a></li>
       <li><a href="hogar.html">Hogar</a></li>
+      <li><a href="electronica.html">Electrónica</a></li>
       <li><a href="carrito.html">Carrito</a></li>
-      <li><a href="#" onclick="cerrarSesion()">Cerrar sesión</a></li>
     </ul>
   `;
-}
-
-function cerrarSesion() {
-  localStorage.removeItem("usuarioLogueado");
-  window.location.href = "login.html";
 }
