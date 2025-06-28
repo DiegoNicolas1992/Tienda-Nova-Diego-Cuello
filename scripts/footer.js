312ad1fc6e9380cfc5c8f9fdd091dc5a0b985081
DiegoNicolas1992 <cuellodiego333@gmail.com>
2025-06-28 12:27:41 -0300

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,1 +1,14 @@
-
+function cargarFooter() {
+  const footer = document.createElement("footer");
+  footer.innerHTML = `
+    <div class="footer-content">
+      <p>&copy; 2025 Tienda Nova - Desarrollado por Diego Cuello</p>
+      <p>Seguinos en:
+        <a href="#">Instagram</a> |
+        <a href="#">Facebook</a> |
+        <a href="#">X</a>
+      </p>
+    </div>
+  `;
+  document.body.appendChild(footer);
+}
